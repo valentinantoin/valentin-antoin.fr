1ec086f7d9a31bcb9c43f2014d328df721dbaf2e
Valentin <valentin.antoin@gmail.com>
2019-09-10 14:01:20 +0200

    Create Portfolio page

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Portfolio extends React.Component {
+    render() {
+        return (
+            <h1>Portfolio Page</h1>
+        )
+    }
+}
+
+export default Portfolio;
