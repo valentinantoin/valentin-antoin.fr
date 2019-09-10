272a63a31f56ed65c26c6d82f9989c5e125214bf
Valentin <valentin.antoin@gmail.com>
2019-09-10 14:25:47 +0200

    Import Navbar in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import './App.css';
-import Navbar from "./components/Navbar";
+import { Layout, Header, Textfield, Drawer, Navigation, Content } from 'react-mdl';
+import { Card, CardTitle, CardText } from 'react-mdl';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faGithub, faLinkedinIn, faTwitter} from "@fortawesome/free-brands-svg-icons";
+import { Link } from 'react-router-dom';
+import Main from './components/Main';
 import FooterSocial from "./components/FooterSocial";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+        <div style={{height: '700px', position: 'relative'}}>
+            <Layout fixedHeader>
+                <Header title="Valentin Antoin">
+                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/valentinantoin"
+                       title="Github"><FontAwesomeIcon icon={faGithub} size="3x" /></a>
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/valentin-antoin/"
+                       title="LinkedIn"><FontAwesomeIcon icon={faLinkedinIn} size="3x" /></a>
+                    <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/Valentin_Antoin"
+                       title="Twitter"><FontAwesomeIcon icon={faTwitter} size="3x" /></a>
+                </Header>
+                <Drawer>
+                    <Card shadow={0} style={{width: 'auto', height: '320px', margin: '0'}}>
+                        <CardTitle expand style={{color: '#fff', background: '#6A7D8A'}}>Valentin Antoin</CardTitle>
+                        <CardText>
+                            Développeur web
+                        </CardText>
+                    </Card>
+                    <Navigation>
+                        <Link to="/presentation">Présentation</Link>
+                        <Link to="/portfolio">Portfolio</Link>
+                        <Link to="/contact">Contact</Link>
+                        <Link to="/">Mon CV</Link>
+                    </Navigation>
+                </Drawer>
+                <Main />
+            </Layout>
+        </div>
       <FooterSocial />
     </div>
   );
