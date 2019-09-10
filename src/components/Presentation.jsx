cccf13d7c479f09a327afca7c106af63db1862b9
Valentin <valentin.antoin@gmail.com>
2019-09-10 14:01:32 +0200

    Create Presentation page

---
+++ src/components/Presentation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Presentation extends React.Component {
+    render() {
+        return (
+            <h1>Presentation Page</h1>
+        )
+    }
+}
+
+export default Presentation;
