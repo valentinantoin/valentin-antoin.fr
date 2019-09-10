a7efe34213b60db5af09bcb649a7ac524155a48d
Valentin <valentin.antoin@gmail.com>
2019-09-10 13:45:22 +0200

    Add card to navbar and change drawer behavior

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Layout, Header, Textfield, Drawer, Navigation, Content } from 'react-mdl';
+import { Card, CardTitle, CardText } from 'react-mdl';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faGithub, faLinkedinIn, faTwitter} from "@fortawesome/free-brands-svg-icons";
 
@@ -9,7 +10,7 @@ class Navbar extends React.Component {
     render() {
         return (
             <div style={{height: '700px', position: 'relative'}}>
-                <Layout fixedHeader fixedDrawer>
+                <Layout fixedHeader>
                     <Header title="Valentin Antoin">
                         <a target="_blank" rel="noopener noreferrer" href="https://github.com/valentinantoin"
                            title="Github"><FontAwesomeIcon icon={faGithub} size="3x" /></a>
@@ -18,7 +19,13 @@ class Navbar extends React.Component {
                         <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/Valentin_Antoin"
                            title="Twitter"><FontAwesomeIcon icon={faTwitter} size="3x" /></a>
                     </Header>
-                    <Drawer title="Menu">
+                    <Drawer>
+                        <Card shadow={0} style={{width: 'auto', height: '320px', margin: '0'}}>
+                            <CardTitle expand style={{color: '#fff', background: '#6A7D8A'}}>Valentin Antoin</CardTitle>
+                            <CardText>
+                                Développeur web
+                            </CardText>
+                        </Card>
                         <Navigation>
                             <a href="/">Présentation</a>
                             <a href="/">Portfolio</a>
