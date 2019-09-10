911b7dcae0f68bf20a295f09272915d935f2ac1b
Valentin <valentin.antoin@gmail.com>
2019-09-10 13:25:57 +0200

    Add social links to footer

--- src/components/FooterSocial.jsx
+++ src/components/FooterSocial.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Footer, FooterSection, FooterLinkList } from 'react-mdl';
+import {Footer, FooterSection, FooterLinkList, Header} from 'react-mdl';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGithub, faLinkedinIn, faTwitter} from "@fortawesome/free-brands-svg-icons";
 
 const FooterSocial = () => {
     return (
         <Footer size="mini">
-            <FooterSection type="left" logo="footer">
+            <FooterSection type="bottom">
                 <FooterLinkList>
-                    <a href="#">option 1</a>
-                    <a href="#">option 2</a>
+                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/valentinantoin"
+                       title="Github"><FontAwesomeIcon icon={faGithub} size="3x" /></a>
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/valentin-antoin/"
+                       title="LinkedIn"><FontAwesomeIcon icon={faLinkedinIn} size="3x" /></a>
+                    <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/Valentin_Antoin"
+                       title="Twitter"><FontAwesomeIcon icon={faTwitter} size="3x" /></a>
                 </FooterLinkList>
             </FooterSection>
         </Footer>
