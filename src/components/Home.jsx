c46eeab985ad1c07fcc51866f91bb42ce9e10fc5
Valentin <valentin.antoin@gmail.com>
2019-09-13 13:37:03 +0200

    Fix portfolio btn

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from 'react-mdl';
+import Button from 'react-bootstrap/Button';
 import {Link} from "react-router-dom";
 
 class Home extends React.Component {
@@ -9,7 +9,7 @@ class Home extends React.Component {
             <div style={{ backgroundImage: 'url(' + img + ')' }} className="main home">
                 <h1 className="mt">Valentin Antoin</h1>
                 <p className="sub">Développeur web</p>
-                <Link to="/portfolio"><Button raised ripple style={{ border: '1px solid #474747' }}>Mon portfolio</Button></Link>
+                <Link to="/portfolio"><Button variant="outline-dark" size='lg' id='btn'>Mon portfolio</Button></Link>
             </div>
         )
     }
