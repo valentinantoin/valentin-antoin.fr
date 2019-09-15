aab0006d0598ff6d87e4b159886ea06a30c9ed55
Valentin <valentin.antoin@gmail.com>
2019-09-15 13:48:10 +0200

    Add tabs to portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,6 +5,8 @@ import Dropdown from "react-bootstrap/Dropdown";
 import {faGithub, faNpm} from "@fortawesome/free-brands-svg-icons";
 import {faGlobe} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
 
 class Portfolio extends React.Component {
     render() {
@@ -12,84 +14,98 @@ class Portfolio extends React.Component {
         return (
             <div style={{ backgroundImage: 'url(' + img + ')' }} className="main portfolio">
 
-                <Card className='card-portfolio'>
-                    <Card.Img variant="top" src="./img/projects/projet1.png" />
-                    <Card.Body>
-                        <Card.Title>WebAgency</Card.Title>
-                        <Card.Text>
-                            HTML / CSS
-                        </Card.Text>
-                        <DropdownButton id="dropdown-basic-button" variant='secondary' title="Liens">
-                            <Dropdown.Item href="http://webagency.valentin-antoin.fr/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGlobe} />   Vers le site</Dropdown.Item>
-                            <Dropdown.Item href="https://github.com/valentinantoin/Webagency" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} />   Vers le code</Dropdown.Item>
-                        </DropdownButton>
-                    </Card.Body>
-                </Card>
-                <Card className='card-portfolio'>
-                    <Card.Img variant="top" src="./img/projects/projet2.png" />
-                    <Card.Body>
-                        <Card.Title>Office de tourisme Strasbourg</Card.Title>
-                        <Card.Text>
-                            WORDPRESS
-                        </Card.Text>
-                        <DropdownButton id="dropdown-basic-button" variant='secondary' title="Liens">
-                            <Dropdown.Item href="http://wordpress.valentin-antoin.fr/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGlobe} />   Vers le site</Dropdown.Item>
-                        </DropdownButton>
-                    </Card.Body>
-                </Card>
-                <Card className='card-portfolio'>
-                    <Card.Img variant="top" src="./img/projects/projet3.png" />
-                    <Card.Body>
-                        <Card.Title>Vélo'V Lyon</Card.Title>
-                        <Card.Text>
-                            HTML / CSS / JS
-                        </Card.Text>
-                        <DropdownButton id="dropdown-basic-button" variant='secondary' title="Liens">
-                            <Dropdown.Item href="http://velov.valentin-antoin.fr/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGlobe} />   Vers le site</Dropdown.Item>
-                            <Dropdown.Item href="https://github.com/valentinantoin/Velo-V" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} />   Vers le code</Dropdown.Item>
-                        </DropdownButton>
-                    </Card.Body>
-                </Card>
-                <Card className='card-portfolio'>
-                    <Card.Img variant="top" src="./img/projects/projet4.png" />
-                    <Card.Body>
-                        <Card.Title>Blog de Jean Forteroche</Card.Title>
-                        <Card.Text>
-                            PHP / MySQL
-                        </Card.Text>
-                        <DropdownButton id="dropdown-basic-button" variant='secondary' title="Liens">
-                            <Dropdown.Item href="https://jeanforteroche.valentin-antoin.fr/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGlobe} />   Vers le site</Dropdown.Item>
-                            <Dropdown.Item href="https://github.com/valentinantoin/Blog-Jean-Forteroche" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} />   Vers le code</Dropdown.Item>
-                        </DropdownButton>
-                    </Card.Body>
-                </Card>
-                <Card className='card-portfolio'>
-                    <Card.Img variant="top" src="./img/projects/projet5.png" />
-                    <Card.Body>
-                        <Card.Title>Portfolio v1.0</Card.Title>
-                        <Card.Text>
-                            SYMFONY
-                        </Card.Text>
-                        <DropdownButton id="dropdown-basic-button" variant='secondary' title="Liens">
-                            <Dropdown.Item href="https://portfoliov1.valentin-antoin.fr/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGlobe} />   Vers le site</Dropdown.Item>
-                            <Dropdown.Item href="https://github.com/valentinantoin/valentin-antoin" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} />   Vers le code</Dropdown.Item>
-                        </DropdownButton>
-                    </Card.Body>
-                </Card>
-                <Card className='card-portfolio'>
-                    <Card.Img variant="top" src="./img/projects/animadio-portfolio.png" />
-                    <Card.Body>
-                        <Card.Title>Animadio-portfolio template</Card.Title>
-                        <Card.Text>
-                            HTML / ANIMADIO CSS FRAMEWORK
-                        </Card.Text>
-                        <DropdownButton id="dropdown-basic-button" variant='secondary' title="Liens">
-                            <Dropdown.Item href="https://portfolio.animadio.org/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGlobe} />   Vers le site</Dropdown.Item>
-                            <Dropdown.Item href="https://github.com/valentinantoin/animadio-portfolio" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} />   Vers le code</Dropdown.Item>
-                            <Dropdown.Item href="https://www.npmjs.com/package/animadio-portfolio" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faNpm} />   Vers le package</Dropdown.Item>
-                        </DropdownButton>
-                    </Card.Body>
-                </Card>
+                <Tabs defaultActiveKey="WebAgency">
+                    <Tab eventKey="WebAgency" title="WebAgency">
+                        <Card className='card-portfolio'>
+                            <Card.Img variant="top" src="./img/projects/projet1.png" />
+                            <Card.Body>
+                                <Card.Title>WebAgency</Card.Title>
+                                <Card.Text>
+                                    HTML / CSS
+                                </Card.Text>
+                                <DropdownButton id="dropdown-basic-button" variant='secondary' title="Liens">
+                                    <Dropdown.Item href="http://webagency.valentin-antoin.fr/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGlobe} />   Vers le site</Dropdown.Item>
+                                    <Dropdown.Item href="https://github.com/valentinantoin/Webagency" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} />   Vers le code</Dropdown.Item>
+                                </DropdownButton>
+                            </Card.Body>
+                        </Card>
+                    </Tab>
+                    <Tab eventKey="Office de tourisme Strasbourg" title="Office de tourisme Strasbourg">
+                        <Card className='card-portfolio'>
+                            <Card.Img variant="top" src="./img/projects/projet2.png" />
+                            <Card.Body>
+                                <Card.Title>Office de tourisme Strasbourg</Card.Title>
+                                <Card.Text>
+                                    WORDPRESS
+                                </Card.Text>
+                                <DropdownButton id="dropdown-basic-button" variant='secondary' title="Liens">
+                                    <Dropdown.Item href="http://wordpress.valentin-antoin.fr/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGlobe} />   Vers le site</Dropdown.Item>
+                                </DropdownButton>
+                            </Card.Body>
+                        </Card>
+                    </Tab>
+                    <Tab eventKey="Vélo'V Lyon" title="Vélo'V Lyon">
+                        <Card className='card-portfolio'>
+                            <Card.Img variant="top" src="./img/projects/projet3.png" />
+                            <Card.Body>
+                                <Card.Title>Vélo'V Lyon</Card.Title>
+                                <Card.Text>
+                                    HTML / CSS / JS
+                                </Card.Text>
+                                <DropdownButton id="dropdown-basic-button" variant='secondary' title="Liens">
+                                    <Dropdown.Item href="http://velov.valentin-antoin.fr/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGlobe} />   Vers le site</Dropdown.Item>
+                                    <Dropdown.Item href="https://github.com/valentinantoin/Velo-V" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} />   Vers le code</Dropdown.Item>
+                                </DropdownButton>
+                            </Card.Body>
+                        </Card>
+                    </Tab>
+                    <Tab eventKey="Blog de Jean Forteroche" title="Blog de Jean Forteroche">
+                        <Card className='card-portfolio'>
+                            <Card.Img variant="top" src="./img/projects/projet4.png" />
+                            <Card.Body>
+                                <Card.Title>Blog de Jean Forteroche</Card.Title>
+                                <Card.Text>
+                                    PHP / MySQL
+                                </Card.Text>
+                                <DropdownButton id="dropdown-basic-button" variant='secondary' title="Liens">
+                                    <Dropdown.Item href="https://jeanforteroche.valentin-antoin.fr/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGlobe} />   Vers le site</Dropdown.Item>
+                                    <Dropdown.Item href="https://github.com/valentinantoin/Blog-Jean-Forteroche" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} />   Vers le code</Dropdown.Item>
+                                </DropdownButton>
+                            </Card.Body>
+                        </Card>
+                    </Tab>
+                    <Tab eventKey="Portfolio v1.0" title="Portfolio v1.0">
+                        <Card className='card-portfolio'>
+                            <Card.Img variant="top" src="./img/projects/projet5.png" />
+                            <Card.Body>
+                                <Card.Title>Portfolio v1.0</Card.Title>
+                                <Card.Text>
+                                    SYMFONY
+                                </Card.Text>
+                                <DropdownButton id="dropdown-basic-button" variant='secondary' title="Liens">
+                                    <Dropdown.Item href="https://portfoliov1.valentin-antoin.fr/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGlobe} />   Vers le site</Dropdown.Item>
+                                    <Dropdown.Item href="https://github.com/valentinantoin/valentin-antoin" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} />   Vers le code</Dropdown.Item>
+                                </DropdownButton>
+                            </Card.Body>
+                        </Card>
+                    </Tab>
+                    <Tab eventKey="Animadio-portfolio" title="Animadio-portfolio">
+                        <Card className='card-portfolio'>
+                            <Card.Img variant="top" src="./img/projects/animadio-portfolio.png" />
+                            <Card.Body>
+                                <Card.Title>Animadio-portfolio template</Card.Title>
+                                <Card.Text>
+                                    HTML / ANIMADIO CSS FRAMEWORK
+                                </Card.Text>
+                                <DropdownButton id="dropdown-basic-button" variant='secondary' title="Liens">
+                                    <Dropdown.Item href="https://portfolio.animadio.org/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGlobe} />   Vers le site</Dropdown.Item>
+                                    <Dropdown.Item href="https://github.com/valentinantoin/animadio-portfolio" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} />   Vers le code</Dropdown.Item>
+                                    <Dropdown.Item href="https://www.npmjs.com/package/animadio-portfolio" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faNpm} />   Vers le package</Dropdown.Item>
+                                </DropdownButton>
+                            </Card.Body>
+                        </Card>
+                    </Tab>
+                </Tabs>
 
             </div>
         )
